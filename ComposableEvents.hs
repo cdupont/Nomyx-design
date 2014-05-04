663cf47b04e7f0cf6b1b895fe72fdb52fb3a9422
Corentin Dupont <corentin.dupont@gmail.com>
2014-05-04 23:30:26 +0200

    example without storage of intermediate results

--- ComposableEvents.hs
+++ ComposableEvents.hs
@@ -1,6 +1,9 @@
 
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE Rank2Types #-}
 
+-- A DSL to represent complex events.
+-- intermediate results are not stored, so all the input must be consumed.
 module Main where
 
 import Control.Applicative
@@ -12,6 +15,7 @@ import Control.Monad.State
 import Safe
 
 type PlayerNumber = Int
+type EventRef = Int
 
 data Event a where
    OnInputText :: PlayerNumber -> Event String   -- A textbox will be created for the player. When filled, this event will fire and return the result
@@ -22,6 +26,7 @@ data Event a where
    EventProduct :: Event (a -> b) -> Event a -> Event b  -- both events should fire, and then the result is returned
    Pure :: a -> Event a  -- Create a fake event. The result is useable with no delay.
    Empty :: Event a -- An event that is never fired. 
+   Ref :: EventRef -> Event a -> Event a
 
 instance Functor Event where
    fmap f a = pure f <*> a
@@ -40,7 +45,7 @@ onInputButton = OnInputButton
 onTime = OnTime
 
 -- A product type
-data MyRecord = MyRecord String Bool
+data MyRecord = MyRecord String Bool deriving Show
 
 -- A sum type
 data MyAlternative = A | B deriving Show
@@ -49,12 +54,6 @@ data MyAlternative = A | B deriving Show
 -- The event callback should be called only when all two events have fired.
 onInputMyRecord :: Event MyRecord
 onInputMyRecord = MyRecord <$> onInputText 1 <*> onInputCheckbox 1
-
--- other possible implementation (given a monad instance)
--- onInputMyRecord' = do
---    s <- onInputText
---    b <- onInputCheckbox
---    return $ MyRecord s b
 
 -- Using the Alternative instance, we build a sum type.
 -- The event callback should be called when the first event have fired.
@@ -85,12 +84,19 @@ data Nomex a where
 
 
 data Game = Game { events :: [EventHandler],
+                   inputResults :: Typeable a => [(EventRef, Maybe a)],
                    outputs :: [String]}
+
+type EventNumber = Int
 
 data EventHandler where 
    EventHandler :: 
-      { event :: Event e,
+      { --eventNumber :: EventNumber,
+        event :: Event e,
         handler :: e -> Nomex ()} -> EventHandler
+
+data Environment i 
+   = Environment { inputs :: [(EventRef, Maybe i)]}
 
 --Evaluation
 evalNomex :: Nomex a -> StateT Game IO a
@@ -101,31 +107,20 @@ evalNomex (Output s) = do
    g <- get
    put $ g { outputs = s : (outputs g)}
    
-
---data InputValues = S String | B Bool | V ()
-
 getInput :: Event a -> IO (Maybe a)
 getInput (OnInputText pn) = do
    putStrLn $ "Player " ++ (show pn) ++ ": enter text"
    s <- getLine
-   return $ readMay s
+   return $ Just s
 getInput (OnInputButton pn) = do
    putStrLn $ "Player " ++ (show pn) ++ ": press b"
    s <- getChar
    return $ if s == 'b' then Just () else Nothing
---case mr of
-  --    Just 'b' -> do
-    --     putStrLn "got b"
-      --   return $ Just ()
-      --Nothing -> return Nothing
 getInput (OnInputCheckbox pn) = do
    putStrLn $ "Player " ++ (show pn) ++ ": enter True/False"
    s <- getLine
    return $ readMay s
-getInput (EventSum a b) = do
-   ia <- getInput a
-   ib <- getInput b
-   return $ ia <|> ib
+getInput (EventSum a b) = (getInput a) `mplus` (getInput b)
 getInput (EventProduct a b) = do
    f <- getInput a
    b <- getInput b
@@ -138,7 +133,6 @@ triggerEvents (EventHandler e h) = do
    mr <- liftIO $ getInput e
    when (isJust mr) $ evalNomex $ h $ fromJust mr
 
-
 runGame :: StateT Game IO ()
 runGame = do
    g <- get
@@ -147,9 +141,9 @@ runGame = do
 
 play :: Nomex ()
 play = do
-   OnEvent onInputMyAlternative (Output . show)
+   OnEvent onInputMyRecord (Output . show)
 
 main = do
-   let g = Game [] []
+   let g = Game [] [] []
    execStateT (evalNomex play >> forever runGame) g
    
