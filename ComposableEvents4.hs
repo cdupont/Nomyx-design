c34a30d154944295d29d30a36565396dedab13ff
kau <kau@kau-Studio-XPS-1647>
2014-05-30 21:54:04 +0200

    .

--- ComposableEvents4.hs
+++ ComposableEvents4.hs
@@ -27,27 +27,28 @@ import Debug.Trace
 type PlayerNumber = Int
 type EventRef = Int
 
-data BEvent a where
-   OnInputText :: PlayerNumber -> BEvent String   -- A textbox will be created for the player. When filled, this event will fire and return the result
-   OnInputCheckbox :: PlayerNumber -> BEvent Bool -- Idem with a checkbox
-   OnInputButton :: PlayerNumber -> BEvent ()     -- Idem with a button
-   OnTime :: UTCTime -> BEvent ()                 -- time event
+data Field a where
+   OnInputText :: PlayerNumber -> Field String   -- A textbox will be created for the player. When filled, this event will fire and return the result
+   OnInputCheckbox :: PlayerNumber -> Field Bool -- Idem with a checkbox
+   OnInputButton :: PlayerNumber -> Field ()     -- Idem with a button
+   OnTime :: UTCTime -> Field ()                 -- time event
    deriving Typeable
+
+deriving instance Eq (Field a)
+deriving instance Show (Field a)
+
+data SomeField = forall a. Typeable a => SomeField (Field a)
 
 data Event a where
    Sum :: Event a -> Event a -> Event a   -- The first event to fire will be returned
    Prod :: Event (a -> b) -> Event a -> Event b -- both events should fire, and then the result is returned
    Pure :: a -> Event a  -- Create a fake event. The result is useable with no delay.
    Empty :: Event a -- An event that is never fired. 
-   Base :: (Typeable a) => BEvent a -> Event a
+   Base :: (Typeable a) => Field a -> Event a
    deriving Typeable
 
 instance Functor Event where
    fmap f a = pure f <*> a
-
-deriving instance Eq (BEvent a)
-
-deriving instance Show (BEvent a)
 
 instance Applicative Event where
    pure = Pure
@@ -56,6 +57,80 @@ instance Applicative Event where
 instance Alternative Event where
    (<|>) = Sum
    empty = Empty
+
+data Nomex a where
+   OnEvent :: Event a -> (a -> Nomex ()) -> Nomex ()
+   Output :: String -> Nomex ()
+
+data Game = Game { _events :: [EventHandler],
+                   _outputs :: [String]}
+
+data EventRes = forall e. Typeable e => EventRes 
+       {bev :: Field e,
+        res :: e}
+
+deriving instance Typeable EventRes 
+
+data EventHandler = forall e. EventHandler 
+      { _event :: Event e,
+        _handler :: e -> Nomex (),
+        _env :: [EventRes]}
+
+instance Show EventHandler where
+   show (EventHandler _ _ en) = show $ length en
+
+makeLenses ''Game
+makeLenses ''EventHandler
+
+--Evaluation
+evalNomex :: Nomex a -> StateT Game IO a
+evalNomex (OnEvent e h) = events %= (EventHandler e h []:)
+evalNomex (Output s) = outputs %= (s:)
+
+updateEH :: EventHandler -> IO (EventHandler)
+updateEH (EventHandler ev h env) = do
+   case (getEventValue ev env) of
+      (BE (Left ebs)) -> do
+         rs <- mapM getRes ebs
+         return (EventHandler ev h (env ++ (catMaybes rs)))
+      (BE (Right _)) -> return $ EventHandler ev h env   
+
+getRes :: SomeField -> IO (Maybe EventRes)
+getRes (SomeField eb) = do
+   mr <- getInputBase eb
+   return $ EventRes eb <$> mr
+
+getInputBase :: Field a -> IO (Maybe a)
+getInputBase (OnInputText pn) = do
+   putStrLn $ "Player " ++ (show pn) ++ ": enter text"
+   Just <$> getLine
+getInputBase (OnInputButton pn) = do
+   putStrLn $ "Player " ++ (show pn) ++ ": press b"
+   s <- getChar
+   return $ if s == 'b' then Just () else Nothing
+getInputBase (OnInputCheckbox pn) = do
+   putStrLn $ "Player " ++ (show pn) ++ ": enter True/False"
+   s <- getLine
+   return $ readMay s
+getInputBase (OnTime t) = do
+   now <- getCurrentTime
+   return $ if now > t then Just () else Nothing
+
+getEventValue :: Event a -> [EventRes] -> BEither [SomeField] a
+getEventValue (Pure a) _ = BE (Right a)
+getEventValue Empty _ = BE (Left [])
+getEventValue (Sum a b) ers = (getEventValue a ers) <|> (getEventValue b ers)
+getEventValue (Prod f b) ers = (getEventValue f ers) <*> (getEventValue b ers)
+getEventValue (Base a) ers = case (lookup' a ers) of
+   Just r -> BE (Right r)
+   Nothing -> BE (Left [SomeField a])
+
+lookup' :: Typeable a => Field a -> [EventRes] -> Maybe a
+lookup' _ [] = Nothing
+lookup' be ((EventRes a r):ers) = case (cast (a,r)) of
+   Just (a',r') -> if (a' == be) then Just r' else lookup' be ers
+   _                      -> lookup' be ers
+
 
 onInputText = Base . OnInputText
 onInputCheckbox = Base . OnInputCheckbox
@@ -78,11 +153,12 @@ onInputMyRecord = MyRecord <$> onInputText 1 <*> onInputCheckbox 1
 onInputMyAlternative :: Event MyAlternative
 onInputMyAlternative = (A <$ onInputButton 1) <|> (B <$ onInputButton 1)
 
-allPlayers = [1 .. 2]
+allPlayers = [1,2]
 
 -- Now complex events can be created, such as voting systems:
 voteEvent :: UTCTime -> Event ([Maybe Bool])
 voteEvent time = sequenceA $ map (singleVote time) allPlayers
+--voteEvent time = liftA [] <$> singleVote time 1 <*> singleVote time 2
 
 singleVote :: UTCTime -> PlayerNumber -> Event (Maybe Bool)
 singleVote timeLimit pn = (Just <$> onInputCheckbox pn) <|> (Nothing <$ onTime timeLimit)
@@ -96,89 +172,9 @@ unanimity = all (== Just True)
 callVote :: UTCTime -> Nomex ()
 callVote t = OnEvent (vote t) (Output . show)
 
-data Nomex a where
-   OnEvent :: Event a -> (a -> Nomex ()) -> Nomex ()
-   Output :: String -> Nomex ()
-
-data Game = Game { _events :: [EventHandler],
-                   _outputs :: [String]}
-
-data EventRes  where
-    EventRes :: (Typeable e) => 
-       {bev :: BEvent e,
-        res :: e} -> EventRes
-
-deriving instance Typeable EventRes 
-
-
-data EventHandler where 
-   EventHandler :: 
-      { _event :: Event e,
-        _handler :: e -> Nomex (),
-        _env :: [EventRes]} -> EventHandler
-
-makeLenses ''Game
-makeLenses ''EventHandler
-
---Evaluation
-evalNomex :: Nomex a -> StateT Game IO a
-evalNomex (OnEvent e h) = events %= (EventHandler e h []:)
-evalNomex (Output s) = outputs %= (s:)
-
-updateEH :: EventHandler -> IO (EventHandler)
-updateEH (EventHandler ev h env) = do
-   case (getEventValue ev env) of
-      (BE (Left ebs)) -> do
-         rs <- mapM getRes ebs
-         return (EventHandler ev h (env ++ (catMaybes rs)))
-      (BE (Right _)) -> return (EventHandler ev h env)   
-
-getRes :: SomeBEvent -> IO (Maybe EventRes)
-getRes (SomeBEvent eb) = do
-   mr <- getInputBase eb
-   return $ EventRes eb <$> mr
-
-updateInput :: (Typeable b) => Event a -> (BEvent b, b) -> Event a
-updateInput (Base a) r = case (cast r) of
-   Just (be, b) | a == be -> Pure b
-   _                      -> Base a
-updateInput (Sum e1 e2) r = Sum (updateInput e1 r) (updateInput e2 r)
-updateInput (Prod f e) r = Prod (updateInput f r) (updateInput e r)
-updateInput (Pure e) _ = Pure e
-updateInput (Empty) _ = Empty
-
--- | an equality that tests also the types.
-(===) :: (Typeable a, Typeable b, Eq b) => a -> b -> Bool
-(===) x y = cast x == Just y
-
-getInputBase :: BEvent a -> IO (Maybe a)
-getInputBase (OnInputText pn) = do
-   putStrLn $ "Player " ++ (show pn) ++ ": enter text"
-   Just <$> getLine
-getInputBase (OnInputButton pn) = do
-   putStrLn $ "Player " ++ (show pn) ++ ": press b"
-   s <- getChar
-   return $ if s == 'b' then Just () else Nothing
-getInputBase (OnInputCheckbox pn) = do
-   putStrLn $ "Player " ++ (show pn) ++ ": enter True/False"
-   s <- getLine
-   return $ readMay s
-getInputBase (OnTime t) = do
-   now <- getCurrentTime
-   return $ if now > t then Just () else Nothing
-
-data SomeBEvent where
-    SomeBEvent :: (Typeable e) => {unEv :: BEvent e} -> SomeBEvent
-
---fromSomeBEvent :: SomeBEvent -> BEvent a
---fromSomeBEvent (SomeBEvent e) = e
-
-deriving instance Show SomeBEvent
-
 newtype BEither a b = BE (Either a b) deriving (Show, Eq, Typeable)
 bLeft = BE . Left
 bRight = BE . Right
-
 
 instance Alternative (BEither [a]) where
    empty        = bLeft []
@@ -188,7 +184,7 @@ instance Alternative (BEither [a]) where
 
 instance Applicative (BEither [a]) where
    pure = BE . Right 
-   BE (Left  f)  <*>  BE (Left b)  =  BE (Left (f ++ b))
+   BE (Left  a)  <*>  BE (Left b)  =  BE (Left (a ++ b))
    BE (Left  e)  <*>  _  =  BE (Left e)
    BE (Right f)  <*>  r  =  fmap f r
 
@@ -196,22 +192,29 @@ instance Functor (BEither a) where
     fmap _ (BE (Left x))  = BE (Left x)
     fmap f (BE (Right y)) = BE (Right (f y))
 
-getEventValue :: Event a -> [EventRes] -> BEither [SomeBEvent] a
-getEventValue (Pure a) _ = BE (Right a)
-getEventValue Empty _ = BE (Left [])
-getEventValue (Sum a b) ers = (getEventValue a ers) <|> (getEventValue b ers)
-getEventValue (Prod f b) ers = (getEventValue f ers) <*> (getEventValue b ers)
-getEventValue (Base a) ers = case (lookup' a ers) of
-   Just r -> BE (Right r)
-   Nothing -> BE (Left [SomeBEvent a])
+main = do
+   let g = Game [] []
+   now <- getCurrentTime
+   execStateT (evalNomex (callVote $ addUTCTime 10 now) >> forever runGame) g
+   --execStateT (evalNomex (OnEvent (sequenceA [onInputMyAlternative, onInputMyAlternative]) (Output . show)) >> forever runGame) g
 
-lookup' :: Typeable a => BEvent a -> [EventRes] -> Maybe a
-lookup' _ [] = Nothing
-lookup' be ((EventRes a r):ers) = case (cast (a,r)) of
-   Just (a',r') -> if (a' == be) then Just r' else Nothing
-   _                      -> lookup' be ers
+updateInputs :: StateT EventHandler IO ()
+updateInputs = do
+   eh <- get 
+   eh' <- liftIO $ updateEH eh
+   put eh'
 
+triggerEvents :: EventHandler -> StateT Game IO ()
+triggerEvents (EventHandler e h env) = do
+   case getEventValue e env of
+      BE (Right a) -> evalNomex $ h a
+      BE (Left _) -> return ()
 
-main = undefined
-
+runGame :: StateT Game IO ()
+runGame = do
+   g <- get
+   liftIO $ putStrLn $ "Output:" ++ (show $ _outputs g)
+   liftIO $ putStrLn $ "Env:" ++ (show $ _events g)
+   zoom (events . traverse) updateInputs
+   mapM_ triggerEvents (_events g)
 
